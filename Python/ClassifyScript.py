9d2b74cdc2301a98fdbea691156ddb044f79b408
Sulantha <sulantha.s@gmail.com>
2015-06-09 19:39:01 -0400

    Adding RUSRFC

--- Python/ClassifyScript.py
+++ Python/ClassifyScript.py
@@ -1,14 +1,21 @@
 __author__ = 'sulantha'
 import numpy
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, partial_dependence
+from sklearn.cross_validation import *
 
 def main():
-    dataset = numpy.genfromtxt('/home/sulantha/PycharmProjects/Conversion/Classification_Table.csv', delimiter=',', usecols=range(1, 20), dtype='f8')[1:]
+    dataset = numpy.genfromtxt('../Classification_Table.csv', delimiter=',', usecols=range(1, 20), dtype='f8')[1:]
     target = [x[0] for x in dataset]
     train = [x[1:] for x in dataset]
 
-    rf = RandomForestClassifier(n_estimators=500)
+    rf = RandomForestClassifier(n_estimators=500, n_jobs = -1, verbose = 1, class_weight = 'auto')
     rf.fit(train, target)
+
+    cv = StratifiedKFold(target, 10)
+    scores = cross_val_score(rf, train, target, cv=cv)
+    print("Accuracy: %0.2f (+/- %0.2f)"
+      % (scores.mean(), scores.std()*2))
+    print(rf.feature_importances_)
 
 
 if __name__=="__main__":
---
+++ Python/FullAnalysis.py
@@ -0,0 +1,34 @@
+__author__ = 'Sulantha'
+import numpy as np
+import pandas as pd
+import scipy as sp
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.cross_validation import cross_val_predict, StratifiedKFold
+import seaborn as sns
+from matplotlib import pyplot as plt
+from sklearn.metrics import roc_curve, auc, confusion_matrix
+
+mci_df = pd.read_csv('../Classification_Table.csv', delimiter=',')
+mci_df = mci_df.drop('ID', axis=1)
+Y = mci_df.Conversion.values
+mci_df = mci_df.drop('Conversion', axis=1)
+X = mci_df.as_matrix()
+
+rf = RandomForestClassifier(n_estimators=1000, n_jobs = -1, verbose = 0, class_weight = 'auto', min_samples_leaf=20)
+cv = StratifiedKFold(Y, 10)
+validated = cross_val_predict(rf, X, Y, cv=cv)
+false_positive_rate, true_positive_rate, thresholds = roc_curve(Y, validated)
+cm = confusion_matrix(Y, validated)
+print(cm)
+# scores = []
+# for val in range(495, 500):
+#     rf = RandomForestClassifier(n_estimators=val, n_jobs = -1, verbose = 1, class_weight = 'auto', max_features = None, oob_score=True)
+#     cv = StratifiedKFold(Y, 10)
+#     validated = cross_val_score(rf, X, Y, cv=cv, scoring='f1')
+#     scores.append(validated)
+#
+# sns.boxplot(scores)
+# plt.xlabel('Number of Trees')
+# plt.ylabel('F1 Score')
+# plt.title('# of Trees vs F1 Score')
+# plt.savefig('foo.png')
