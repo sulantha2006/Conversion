0092dc489c820367466e8aa61a7987eae29e1d60
sulantha <sulantha.s@gmail.com>
2015-06-12 10:23:11 -0400

    :-)

--- Python/RUSRandomForest/Config.py
+++ Python/RUSRandomForest/Config.py
@@ -1,3 +1,27 @@
 __author__ = 'Sulantha'
 
 figOutputPath = '/home/sulantha/Desktop/Classification/ROCs/'
+
+all_list = ['Age_bl', 'PTGENDER', 'APOE_bin', 'PTAU181P_bl', 'PTAU_Pos', 'ABETA142', 'ABETA142_Pos',
+                 'PTAU_AB142_Ratio', 'AV45_bl_Global_SUVR_NEW', 'FDG_bl_Global_SUVR_NEW', 'AV45_region1', 'AV45_region2',
+                 'AV45_region3', 'AV45_region4', 'FDG_region1', 'FDG_region2', 'FDG_region3',
+                'FDG_region4', 'FDG_region5']
+xticks_dict = dict(Age_bl='Age',
+                   PTGENDER='Gender',
+                   APOE_bin='APOE',
+                   PTAU181P_bl='CSF PTAU',
+                   PTAU_Pos='CSF PTAU > 23',
+                   ABETA142='CSF ABETA',
+                   ABETA142_Pos='CSF ABETA > 192',
+                   PTAU_AB142_Ratio='PTAU ABETA Ratio',
+                   AV45_bl_Global_SUVR_NEW='AV45 Global SUVR',
+                   FDG_bl_Global_SUVR_NEW='FDG Global SUVR',
+                   AV45_region1='AV45 Sup. Tem. Gyrus',
+                   AV45_region2='AV45 Basal Tem. Lobe',
+                   AV45_region3='AV45 OFC (Basal)',
+                   AV45_region4='AV45 Angular Gyrus',
+                   FDG_region1='FDG Precuneus',
+                   FDG_region2='FDG PCC',
+                   FDG_region3='FDG OFC',
+                   FDG_region4='FDG Piriform C.',
+                   FDG_region5='FDG Hippocampus')
