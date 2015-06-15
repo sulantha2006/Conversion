77d91a86698bfffa49b45b3e3f28b22420c1b5a9
sulantha <sulantha.s@gmail.com>
2015-06-15 13:48:58 -0400

    :-)

--- Python/RUSRandomForest/plotROCFromFile.py
+++ Python/RUSRandomForest/plotROCFromFile.py
@@ -19,17 +19,29 @@ def plotROC(fpr, tpr, th, legend_title, plot_title, fileName):
     plt.ylim([-0.01, 1.01])
     optY, optX, optTH = getOptimalOparatingPoint(fpr, tpr, th)
     plt.plot(optX, optY, 'o')
-    plt.annotate('Sen : {0}\nSpec: {1}\nTh: {2}'.format(optY, 1-optX, optTH), annotation_clip=dict(size='small'), xy=(optX, optY), xytext=(0.25, 0.75), arrowprops=dict(arrowstyle='fancy', fc='0.6', ec='none', shrinkB=5, connectionstyle="angle3,angleA=0,angleB=-45"))
+    plt.annotate('Sen : {0}\nSpec: {1}\nTh: {2}'.format(optY, 1-optX, optTH), xy=(optX, optY), xytext=(0.25, 0.75), arrowprops=dict(arrowstyle='fancy', fc='0.6', ec='none', shrinkB=5, connectionstyle="angle3,angleA=0,angleB=-45"))
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
     plt.title(plot_title)
-    plt.legend(loc="lower right", prop={'size':'small', 'family':'sans-serif'})
+    plt.legend(loc="lower right", prop={'size':'small', 'family':'sans'})
     plt.tight_layout()
     plt.savefig(Config.figOutputPath + fileName)
 
 if __name__ == '__main__':
     data = numpy.loadtxt(Config.figOutputPath+'CSF_ONLY_SensSpec.csv', delimiter=',')
-    tpr = data[:, 1]
-    fpr = 1-data[:, 0]
-    th = data[:, 2]
-    plotROC(fpr, tpr, th, 'CSF ONLY', 'ROC for MCI Converters (CSF)', 'CSF_ONLY_ROC.png')
+    plotROC(1-data[:, 0], data[:, 1], data[:, 2], 'CSF ONLY', 'ROC for MCI Converters (CSF)', 'CSF_ONLY_ROC.png')
+
+    data = numpy.loadtxt(Config.figOutputPath+'AV45_ONLY_SensSpec.csv', delimiter=',')
+    plotROC(1-data[:, 0], data[:, 1], data[:, 2], 'AV45 ONLY', 'ROC for MCI Converters (AV45)', 'AV45_ONLY_ROC.png')
+
+    data = numpy.loadtxt(Config.figOutputPath+'FDG_ONLY_SensSpec.csv', delimiter=',')
+    plotROC(1-data[:, 0], data[:, 1], data[:, 2], 'FDG ONLY', 'ROC for MCI Converters (FDG)', 'FDG_ONLY_ROC.png')
+
+    data = numpy.loadtxt(Config.figOutputPath+'CSF_AV45_SensSpec.csv', delimiter=',')
+    plotROC(1-data[:, 0], data[:, 1], data[:, 2], 'CSF AV45', 'ROC for MCI Converters (CSF & AV45)', 'CSF_AV45_ROC.png')
+
+    data = numpy.loadtxt(Config.figOutputPath+'CSF_FDG_SensSpec.csv', delimiter=',')
+    plotROC(1-data[:, 0], data[:, 1], data[:, 2], 'CSF FDG', 'ROC for MCI Converters (CSF & FDG)', 'CSF_FDG_ROC.png')
+
+    data = numpy.loadtxt(Config.figOutputPath+'ALL_SensSpec.csv', delimiter=',')
+    plotROC(1-data[:, 0], data[:, 1], data[:, 2], 'ALL', 'ROC for MCI Converters (ALL)', 'ALL_ROC.png')
